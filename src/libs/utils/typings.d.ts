cdefd25afc61a0ba59029d9f949f84fb072f9814
razor <acm09@yandex.ru>
2025-02-01 13:51:50 +0300

    update

---
+++ src/libs/utils/typings.d.ts
@@ -0,0 +1,17 @@
+export type StringOrNumber = string | number
+
+export type StringOrBoolean = string | boolean
+
+export type StringOrNumberOrBoolean = StringOrNumber | boolean
+
+export type Primitive = Nullable<StringOrNumberOrBoolean>
+
+export type PlainObject<T = unknown> = Record<string, T>
+
+export type PlainObjectOrArray<T = unknown, R = unknown> = PlainObject<T> | Array<R>
+
+export type Nullable<T> = T | null
+
+export type Optional<T> = T | undefined
+
+export type NullableProperties<T> = Record<keyof T, Nullable<T[keyof T]>>
